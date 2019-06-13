1eb2585961a34e09e54e16a07f9df48591da3630
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-13 17:44:02 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop
+  array.pop
+end
+
+def pop_with_args(array, num)
+  array.pop(num)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array, num)
+  array.shift(num)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  array.insert()
+
+
+
+
+
+
+
+
+
+
+
+
+
